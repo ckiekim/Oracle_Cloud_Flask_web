c8eef36a879c92df00f6f4e20918b327a1c9f1c0
Cheolkie Kim <ckiekim@naver.com>
2021-09-11 14:57:12 +0900

    초기 화면

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template, session, request, g
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello Flask!!!'
+    menu = {'ho':1, 'da':0, 'ml':0, 
+            'se':0, 'co':0, 'cg':0, 'cr':0, 'wc':0,
+            'cf':0, 'ac':0, 're':0, 'cu':0}
+    return render_template('index.html', menu=menu)
 
 if __name__ == '__main__':
-    # app.run(debug=True)
-    app.run(host="0.0.0.0", port=int("5000"), debug=True)
+    app.run(debug=True)
