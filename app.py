9c2f17efd080e4d729d686fda5279d88ab0cfd93
Cheolkie Kim <ckiekim@naver.com>
2021-09-11 16:49:48 +0900

    날씨 추가

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 from flask import Flask, render_template, session, request, g
+from my_util.weather import get_weather
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     menu = {'ho':1, 'da':0, 'ml':0, 
-            'se':0, 'co':0, 'cg':0, 'cr':0, 'wc':0,
+            'se':0, 'co':0, 'cg':0, 'cr':0, 'wc':0, 'rs':0,
             'cf':0, 'ac':0, 're':0, 'cu':0}
-    return render_template('index.html', menu=menu)
+    return render_template('index.html', menu=menu, weather=get_weather())
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
