981d7cbcf3858ff0cb7a096a54fbef73be164041
Cheolkie Kim <ckiekim@naver.com>
2021-09-12 22:24:21 +0900

    크롤링

--- app.py
+++ app.py
@@ -4,10 +4,12 @@ from logging.config import dictConfig
 import json, logging
 from bp1_seoul.seoul import seoul_bp
 from bp2_cartogram.carto import carto_bp
+from bp3_crawling.crawl import crawl_bp
 
 app = Flask(__name__)
 app.register_blueprint(seoul_bp, url_prefix='/seoul')
 app.register_blueprint(carto_bp, url_prefix='/cartogram')
+app.register_blueprint(crawl_bp, url_prefix='/crawling')
 
 with open('./log/logging.json', 'r') as file:
     config = json.load(file)
---
+++ bp3_crawling/crawl.py
@@ -0,0 +1,42 @@
+from flask import Blueprint, render_template, request, session
+from flask import current_app, redirect, url_for
+from fbprophet import Prophet
+from datetime import datetime, timedelta
+import os
+import pandas as pd
+from my_util.weather import get_weather
+import my_util.crawl_util as cu
+
+crawl_bp = Blueprint('crawl_bp', __name__)
+menu = {'ho':0, 'da':1, 'ml':0, 
+        'se':0, 'cg':0, 'cr':1, 'wc':0, 'rs':0,
+        'cf':0, 'ac':0, 're':0, 'cu':0, 'nl':0}
+
+@crawl_bp.route('/food', methods=['GET', 'POST'])
+def food():
+    if request.method == 'GET':
+        place = request.args.get('place', '성복역')
+        rest_list = cu.siksin(place)
+        return render_template('crawling/food.html', menu=menu, weather=get_weather(),
+                                rest_list=rest_list, place=place)
+    else:
+        place = request.form['place']
+        return redirect(url_for('crawl_bp.food')+f'?place={place}')
+
+@crawl_bp.route('/music')
+def music():
+    music_list = cu.genie()
+    return render_template('crawling/music.html', menu=menu, weather=get_weather(),
+                            music_list=music_list)
+
+@crawl_bp.route('/music_jquery')
+def music_jquery():
+    music_list = cu.genie()
+    return render_template('crawling/music_jquery.html', menu=menu, weather=get_weather(),
+                            music_list=music_list)
+
+@crawl_bp.route('/book')
+def book():
+    book_list = cu.interpark()
+    return render_template('crawling/book.html', menu=menu, weather=get_weather(),
+                            book_list=book_list)
