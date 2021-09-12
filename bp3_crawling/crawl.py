1357b6f638c23cbbe97a48ab29822a14b785f97a
Cheolkie Kim <ckiekim@naver.com>
2021-09-12 22:26:42 +0900

    크롤링

--- bp3_crawling/crawl.py
+++ bp3_crawling/crawl.py
@@ -1,6 +1,5 @@
 from flask import Blueprint, render_template, request, session
 from flask import current_app, redirect, url_for
-from fbprophet import Prophet
 from datetime import datetime, timedelta
 import os
 import pandas as pd
