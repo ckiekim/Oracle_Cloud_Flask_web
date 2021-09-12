961fa37dda7110815ff34c12797e2a34197ecd74
Cheolkie Kim <ckiekim@naver.com>
2021-09-12 13:57:43 +0900

    지역분석(서울)

--- app.py
+++ app.py
@@ -2,8 +2,10 @@ from flask import Flask, render_template, session, request, g
 from my_util.weather import get_weather
 from logging.config import dictConfig
 import json, logging
+from bp1_seoul.seoul import seoul_bp
 
 app = Flask(__name__)
+app.register_blueprint(seoul_bp, url_prefix='/seoul')
 
 with open('./logging.json', 'r') as file:
     config = json.load(file)
@@ -12,8 +14,8 @@ dictConfig(config)
 @app.route('/')
 def index():
     menu = {'ho':1, 'da':0, 'ml':0, 
-            'se':0, 'co':0, 'cg':0, 'cr':0, 'wc':0, 'rs':0,
-            'cf':0, 'ac':0, 're':0, 'cu':0}
+            'se':0, 'cg':0, 'cr':0, 'wc':0, 'rs':0,
+            'cf':0, 'ac':0, 're':0, 'cu':0, 'nl':0}
     client_addr = request.environ.get('HTTP_X_REAL_IP', request.remote_addr)
     logging.debug(f'Connected to {client_addr}')
     return render_template('index.html', menu=menu, weather=get_weather())
