ee56a60942c599f497ec7a846f431d120a7d4c1d
Cheolkie Kim <ckiekim@naver.com>
2021-09-15 23:05:49 +0900

    추천시스템

--- bp5_recommendation/movie_rcmd.py
+++ bp5_recommendation/movie_rcmd.py
@@ -1,13 +1,16 @@
 import pandas as pd
-import warnings, joblib
+import warnings
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import linear_kernel
 
 warnings.filterwarnings('ignore')
 df = pd.read_csv('../static/data/movies_meta_summary.csv')
 df = df.head(20000)
+tvect = TfidfVectorizer(stop_words='english')
+tfidf_matrix = tvect.fit_transform(df.overview)
 indices = pd.Series(df.index, index=df.title).drop_duplicates()
-print('start loading')
-cos_sim = joblib.load('../static/data/movie_cos_sim.pkl')
-print('loaded')
+
+cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
 
 def get_recommendations(title, cosine_sim=cosine_sim):
     idx = indices[title]
