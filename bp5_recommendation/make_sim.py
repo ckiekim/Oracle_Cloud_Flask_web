863495e8164848aff20923dd665e89570934efa0
Cheolkie Kim <ckiekim@naver.com>
2021-09-15 22:19:56 +0900

    추천시스템

--- bp5_recommendation/make_sim.py
+++ bp5_recommendation/make_sim.py
@@ -5,8 +5,12 @@ from sklearn.metrics.pairwise import linear_kernel
 
 warnings.filterwarnings('ignore')
 df = pd.read_csv('../static/data/movies_meta_summary.csv')
+df = df.head(20000)
 tvect = TfidfVectorizer(stop_words='english')
 tfidf_matrix = tvect.fit_transform(df.overview)
 indices = pd.Series(df.index, index=df.title).drop_duplicates()
+
+print('Starting ...')
 cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
+print('similarity')
 joblib.dump(cosine_sim, '../static/data/movie_cos_sim.pkl')
